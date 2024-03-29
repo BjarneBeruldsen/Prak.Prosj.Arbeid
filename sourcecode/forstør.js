42c77ab65177ca8fbe308837ffcf708c38eb710b
BjarneBeruldsen <127053583+BjarneBeruldsen@users.noreply.github.com>
2024-03-29 09:16:08 +0100

    Delete sourcecode/forstør.js

--- sourcecode/forstør.js
+++
@@ -1,5 +0,0 @@
-const imageContainer = document.getElementById('image-container');
-
-imageContainer.addEventListener('click', function() {
-  this.classList.toggle('enlarged'); // Bytter klassen "enlarged" når bildet blir klikket på
-});
